992e507f5c1edcec6489c3077bb1b8240e631203
munem91 <borisukrusaln12345@gmail.com>
2022-11-05 22:33:54 +0300

    add LinearProgressIndicator and FloatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(const MySecondApp());
+
+class MySecondApp extends StatelessWidget {
+  const MySecondApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.indigo,
         appBar: AppBar(
           title: const Text('My Second App'),
           centerTitle: true,
         ),
-        body: const Center(
-          child: Text(
-            'Body Text',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                LinearProgressIndicator(value: 23),
+                Text(
+                  '23%',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                Text(
+                  'Press button to downoload%',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ],
             ),
           ),
         ),
+        floatingActionButton: const FloatingActionButton(
+          onPressed: null,
+          child: Icon(Icons.download),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
